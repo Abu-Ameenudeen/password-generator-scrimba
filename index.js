0aec32b4e09b8f5e5b3ad8f9c7cf25bbbc4724d1
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2025-11-04 15:55:20 +0530

    Added Password Generation
    - Updated HTML to get password
    - Fixed height in password text area
    - Added get & generate password function in index.js file

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let passwordLength = 15;
+let password1El = document.getElementById("password1");
+let password2El = document.getElementById("password2");
+let generateBtn = document.getElementById("generate-btn");
+ 
+function getPassword() {
+    password1El.textContent = generatePassword();
+    password2El.textContent = generatePassword();
+};
+
+function generatePassword() {
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+    return password;
+}           
